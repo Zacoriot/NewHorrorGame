b1dd1c1c7379e5554e11eb42887ea3253145cf64
Zacoriot <isaacjbrooks@gmail.com>
2024-11-22 10:34:02 +0000

    Made player movement and gravity

--- Source/Game/Player/PlayerMovementController.cs
+++ Source/Game/Player/PlayerMovementController.cs
@@ -7,8 +7,39 @@ namespace Game;
 /// </summary>
 public class PlayerMovementController : Script
 {
+    [Header("==MOVEMENT==")]
+    CharacterController _CharacterController;
+    [ShowInEditor, Serialize] float _MovementSpeed;
+    float _FallingSpeed;
+
+    float _Gravity;
+
+    public override void OnStart()
+    {
+        _CharacterController = (CharacterController)Actor;
+
+        _Gravity = Physics.Gravity.Y;
+    }
+
     public override void OnUpdate()
     {
-        Debug.Log(InputManager.GetMovementAxis());
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        if(!_CharacterController.IsGrounded)
+        {
+            _FallingSpeed += _Gravity * Time.DeltaTime;
+        }
+
+        Vector3 moveInput = Actor.Transform.Forward * InputManager.GetMovementAxis().Y +
+                                    Actor.Transform.Right * InputManager.GetMovementAxis().X;
+
+        moveInput = moveInput.Normalized * _MovementSpeed;
+
+        Vector3 finalMovement = moveInput + new Vector3(0, _FallingSpeed, 0);
+
+        _CharacterController.Move(finalMovement * Time.DeltaTime);
     }
 }
