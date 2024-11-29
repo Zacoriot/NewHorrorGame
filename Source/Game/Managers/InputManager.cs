43c8ed8825c582f689359392c5622fdd5b50ce3e
Zacoriot <isaacjbrooks@gmail.com>
2024-11-29 10:04:51 +0000

    Camera Controlls

--- Source/Game/Managers/InputManager.cs
+++ Source/Game/Managers/InputManager.cs
@@ -10,6 +10,8 @@ public class InputManager : Script
     // INPUT AXIS
     static InputAxis _VerticalMovementAxis;
     static InputAxis _HorizontalMovementAxis;
+    static InputAxis _MouseX;
+    static InputAxis _MouseY;
 
     // INPUT ACTIONS
 
@@ -18,6 +20,19 @@ public class InputManager : Script
         // INPUT AXIS
         _VerticalMovementAxis = new InputAxis("Vertical");
         _HorizontalMovementAxis = new InputAxis("Horizontal");
+        _MouseX = new InputAxis("Mouse X");
+        _MouseY = new InputAxis("Mouse Y");
+
+        // INPUT ACTIONS
+    }
+
+    public override void OnDestroy()
+    {
+        // INPUT AXIS
+        _VerticalMovementAxis.Dispose();
+        _HorizontalMovementAxis.Dispose();
+        _MouseX.Dispose();
+        _MouseY.Dispose();
 
         // INPUT ACTIONS
     }
@@ -26,5 +41,10 @@ public class InputManager : Script
     public static Vector2 GetMovementAxis()
     {
         return new Vector2(_HorizontalMovementAxis.ValueRaw, _VerticalMovementAxis.ValueRaw);
+    }
+
+    public static Vector2 GetMouseAxis()
+    {
+        return new Vector2(_MouseX.ValueRaw, _MouseY.ValueRaw);
     }
 }
