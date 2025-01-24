3d32ea0ab65b5099b0392729d616e590844c4380
Isaac Brooks <isaacjbrooks@gmail.com>
2025-01-24 09:57:56 +0000

    interact

---
+++ Source/Game/Interaction/MagicDisapearingBox.cs
@@ -0,0 +1,14 @@
+﻿using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// MagicDisapearingBox Script.
+/// </summary>
+public class MagicDisapearingBox : Interactable
+{
+    public override void Interact()
+    {
+        Destroy(Actor);
+    }
+}
